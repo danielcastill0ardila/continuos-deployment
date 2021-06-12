ac587eabf5003758842277af86d38194602e2608
danielcastill0ardila <danielcastilloardila@hotmail.com>
2021-06-12 11:34:51 -0500

    finish

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,5 +1,8 @@
 
 cd /home/ec2-user/app
+
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 npm install
