c8a62d7eadc0cae4f43056698aac23dfc875d4ff
danielcastill0ardila <danielcastilloardila@hotmail.com>
2021-06-11 23:55:28 -0500

    updated index message

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const PORT = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/html");
-  res.end(`<h1>Hello, World!</h1> ${req.url}`);
+  res.end(`<h1>Hello!</h1> ${req.url}`);
 });
 
 server.listen(PORT, () => {
