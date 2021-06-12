7a5799d771ae18a388bc749ddf23ae0e411bb060
danielcastill0ardila <danielcastilloardila@hotmail.com>
2021-06-12 13:24:45 -0500

    allow write permissions

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -4,6 +4,9 @@ wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+
+sudo chmod -R 777 /home/ec2-user/app
+
 cd /home/ec2-user/app
 
 npm install
