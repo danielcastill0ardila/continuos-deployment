478138c24e11f3a4ae9613f7b9d0e419077ee409
danielcastill0ardila <danielcastilloardila@hotmail.com>
2021-06-12 01:50:12 -0500

    finish configuration

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,7 +1,7 @@
-source /home/ec2-user/.bash_profile
-
+cd /home/ec2-user/app
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
+pm2 delete all
+pm2 save --force
 pm2 start ../index.js
