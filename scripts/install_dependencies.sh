a2239a942019a4f9bdb9ae786b041c9ff8901241
danielcastill0ardila <danielcastilloardila@hotmail.com>
2021-06-12 11:42:35 -0500

    updated scripts

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -5,7 +5,7 @@ wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-npm install
+
 npm install pm2 -g
 
 
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,6 +3,6 @@ cd /home/ec2-user/app
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
+npm install
 pm2 delete ${APP_NAME}
 pm2 start index.js --name ${APP_NAME}
