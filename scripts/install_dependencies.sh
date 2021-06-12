0f8995b49a2191c4cfad18c256cf68d1831e1590
danielcastill0ardila <danielcastilloardila@hotmail.com>
2021-06-12 12:00:37 -0500

    finish process

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,11 +1,12 @@
 
-cd /home/ec2-user/app
 
 wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+cd /home/ec2-user/app
 
+npm install
 npm install pm2 -g
 
 
