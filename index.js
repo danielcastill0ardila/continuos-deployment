dea3241e0b214ca5366c570e69f8909a208c1702
danielcastill0ardila <danielcastilloardila@hotmail.com>
2021-06-12 02:43:49 -0500

    updated endpoint response

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const http = require("http");
 const PORT = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader("Content-Type", "text/html");
-  res.end(`<h1>Hello!</h1> ${req.url}`);
+  res.setHeader("Content-Type", "application/json");
+  res.end(`<h1>Hello new module!</h1> ${req.url}`);
 });
 
 server.listen(PORT, () => {
