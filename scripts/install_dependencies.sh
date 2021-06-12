3ca436cc5dd751d7b8b9508f85acfd8fec36d09c
danielcastill0ardila <danielcastilloardila@hotmail.com>
2021-06-12 11:26:53 -0500

    sudo access1

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,7 +1,7 @@
 
-
+cd /home/ec2-user/app
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 npm install pm2 -g
-cd /home/ec2-user/app
+
 sudo npm install
