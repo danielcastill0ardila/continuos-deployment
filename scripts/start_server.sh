b01e824ae906ae603b1e479deb5731accdeab343
danielcastill0ardila <danielcastilloardila@hotmail.com>
2021-06-12 02:40:42 -0500

    updated start server

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,6 +2,5 @@ cd /home/ec2-user/app
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-pm2 delete all
-pm2 save --force
+
 pm2 start index.js
