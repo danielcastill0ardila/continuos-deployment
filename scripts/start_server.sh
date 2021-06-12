1d06f2bb1304dc70dae64eeb4415afbe6af3e60b
danielcastill0ardila <danielcastilloardila@hotmail.com>
2021-06-12 02:55:17 -0500

    nice start server file

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,6 +1,8 @@
+APP_NAME='web-api'
 cd /home/ec2-user/app
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-pm2 startOrRestart index.js
+pm2 delete ${APP_NAME}
+pm2 start index.js --name ${APP_NAME}
