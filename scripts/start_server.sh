8d50516cabc24ec42f27c52b8cf45a7a58146030
danielcastill0ardila <danielcastilloardila@hotmail.com>
2021-06-12 11:54:44 -0500

    finish

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,5 +1,5 @@
 APP_NAME='web-api'
-cd /home/ec2-user/app
+cd /home/ec2-user/app/
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
