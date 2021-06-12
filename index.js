ee32b3e47928ffaaedbf19815a2eba03c507d017
danielcastill0ardila <danielcastilloardila@hotmail.com>
2021-06-11 21:55:38 -0500

    Built a simple http server with core module

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("aja");
+const http = require("http");
+const PORT = 3000;
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/html");
+  res.end(`<h1>Hello, World!</h1> ${req.url}`);
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running at port ${PORT}`);
+});
