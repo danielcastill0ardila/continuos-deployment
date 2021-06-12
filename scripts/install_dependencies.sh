ce09cc12ea32730aec3a74be11620bf4851e0186
danielcastill0ardila <danielcastilloardila@hotmail.com>
2021-06-12 13:31:14 -0500

    update script

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -6,7 +6,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 
 sudo chmod -R 777 /home/ec2-user/app
-
+nvm use node
 cd /home/ec2-user/app
 
 npm install
